5407a219fc302592e0182a3f2970f370fcf88b8d
Tola-lemma <tolalemma@gmail.com>
2023-03-23 15:10:46 +0300

    social media is added

---
+++ src/Portfolio_Container/Home/Profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Profile = () => {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://m.facebook.com/m.tolalemma">
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a href="https://www.instagram.com/tolalemma/">
+              <i class="fa-brands fa-square-instagram"></i>
+            </a>
+            <a href="https://mobile.twitter.com/tolalemma">
+              <i class="fa-brands fa-twitter"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/tolalemma/">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://t.me/tolalemma">
+              <i class="fa-brands fa-telegram"></i>
+            </a>
+            <a href="https://call.imo.im/1optimisticboy">
+              <i class="fa-brands fa-google-plus"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
