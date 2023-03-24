9fbaced94f8ef3333be92437cdb262f132c9369a
Tola-lemma <tolalemma@gmail.com>
2023-03-24 11:37:47 +0300

    Footer is rendered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Footer } from "./Portfolio_Container/Home/Footer/Footer";
 import { Profile } from "./Portfolio_Container/Home/Profile";
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Footer />
     </div>
   );
 }
