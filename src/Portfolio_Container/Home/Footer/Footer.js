7f9ff79586727955fdbd6dd79fe1f6a1f8de04a9
Tola-lemma <tolalemma@gmail.com>
2023-03-24 11:37:01 +0300

    Footer component is added iwith bg images

---
+++ src/Portfolio_Container/Home/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+import shapebg from '../../../assets/Home/shape-bg.png'
+export const  Footer=()=> {
+  return (
+    <div className='footer-container'>
+        <div className='footer-parent'>
+        <img src={shapebg} alt="shape background"/>
+        </div>
+    </div>
+  )
+}
