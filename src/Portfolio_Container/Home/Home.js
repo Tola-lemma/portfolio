69ac875b2bd08feb26eec15f9ad9a0f870f7dadb
Tola-lemma <tolalemma@gmail.com>
2023-03-24 23:36:25 +0300

    refactor:restructured

--- src/Portfolio_Container/Home/Home.js
+++ src/Portfolio_Container/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Footer } from './Footer/Footer'
-import { Profile } from './Profile'
 import './Home.css'
+import { Profile } from './Profile/Profile'
 export const Home = ()=> {
   return (
     <div className='home-container'>
