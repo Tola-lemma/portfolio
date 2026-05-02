46e4c338fca5d19b88f55fee86d84fcf3e932b2c
Tola Lemma <tolalemma@gmail.com>
2026-05-02 18:52:24 +0300

    Feat:New web service

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import PortfolioContainer from "./Portfolio_Container/PortfolioContainer";
 import axios from "axios";
-axios.defaults.baseURL = "https://portfolio-tolalemma-ka07.onrender.com";
+axios.defaults.baseURL = "https://portfolio-tolalemmaweb.onrender.com";
 function App() {
   return (
     <div className="App">
