fc1471f03e68c28d38eec4a82f91d27436288ef9
Tola Lemma <tolalemma@gmail.com>
2026-05-02 20:36:13 +0300

    Feat:vercel build

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import PortfolioContainer from "./Portfolio_Container/PortfolioContainer";
 import axios from "axios";
-axios.defaults.baseURL = "https://portfolio-tolalemmaweb.onrender.com";
+axios.defaults.baseURL = "https://portfolio-webserver-eight.vercel.app/";
 function App() {
   return (
     <div className="App">
